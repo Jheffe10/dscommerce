b16c4ac2a5041a906d9f499ec4e5deac1cfcb361
Jheffe10 <jheffeguerreiro@yahoo.com.br>
2026-01-24 08:17:22 -0300

    Controle de acesso por perfil e rota

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -138,7 +139,7 @@ public class ProductController {
 		List<ProductDTO> listDto = service.findAllNaoPaginada();
 		return ResponseEntity.ok(listDto);
 	}
-
+	
 	@GetMapping //parâmetro de consulta name - é opcional, se não for passado na consulta adota o padrão ""
 	public ResponseEntity<Page<ProductDTO>> findAll(@RequestParam(name = "name", defaultValue="") String name, @PageableDefault(size = 12) Pageable pageable) {
 		Page<ProductDTO> listDto = service.findAll(name, pageable);
@@ -146,6 +147,7 @@ public class ProductController {
 	}
 
 	// Inserir produto
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 	@PostMapping
 	public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto) { //@Valid para fazer as verificações constantes no ProductDTO
 		dto = service.insert(dto);
@@ -160,6 +162,7 @@ public class ProductController {
 		// Posso jogar esse caminho na busca por id no postman, vai retornar esse recurso criado.
 	}
 
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 	// Update
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductDTO dto) { // @Valid deve vir na frente do dto
@@ -168,6 +171,7 @@ public class ProductController {
 		return ResponseEntity.ok(dto); // vai retorna o código 200 OK, o corpo dto
 	}
 
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 	// Delete
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id) { // @PathVariable é o que vem na rota da requisição
